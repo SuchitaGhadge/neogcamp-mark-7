a43d8dce0045b52bc99f04fc2554fb6b86336814
SuchitaGhadge <suchita8494.sg@gmail.com>
2020-12-24 01:30:16 +0530

    JS added

---
+++ app.js
@@ -0,0 +1,32 @@
+var txtInput = document.querySelector('#input-txt');
+var btnTranslate = document.querySelector('#btn-translate');
+var outputDiv = document.querySelector('#output-txt');
+
+// server link
+serverURL = 'https://api.funtranslations.com/translate/quenya.json'
+
+function getTransaltionURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error occured!" + error )
+    alert("Somethinng went wrong. Plz try after some time!")
+}
+
+function clickHandler(){
+    // taking input
+    var inputText = txtInput.value;
+    
+    // calling server for processing
+    fetch(getTransaltionURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        //error handling
+        .catch(errorHandler) 
+}
+
+btnTranslate.addEventListener("click", clickHandler);
